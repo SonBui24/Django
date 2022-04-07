ba61fdeecfe484cc1a5437150673f8c7d92e98e9
SonBui24 <son24051994@gmail.com>
2022-04-07 21:04:42 +0700

    Lesson05

--- Lesson05/05-03.py
+++ Lesson05/05-03.py
@@ -1,6 +1,6 @@
 """
 - objects.create(): Dùng để vừa tạo đối tượng vừa cập nhật luôn vào DB.
-- objects.update(): Dùng để update toàn bộ column
+- objects.update(): Dùng để update DB
 - objects.get_or_create(): Dùng khi ta chưa biết dữ liệu đã có hay chưa. Nếu có thì lấy ra, chưa thì thêm vào DB.
 """
 
